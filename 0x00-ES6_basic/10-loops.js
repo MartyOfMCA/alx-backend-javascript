886aa7270f360a0cf6f103e9d2b2352c3d08d9f8
Martin <martyofmca@gmail.com>
2024-01-03 23:16:22 +0000

    return new array having items in an array with appended string

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (var idx in array) {
-    var value = array[idx];
-    array[idx] = appendString + value;
+  const newArray = [];
+
+  for (const val of array) {
+    newArray.push(`${appendString}${val}`);
   }
 
-  return array;
+  return newArray;
 }
