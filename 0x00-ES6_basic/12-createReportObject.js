9b875f659e1522b6d7e18caaef68ab900d17ab63
Martin <martyofmca@gmail.com>
2024-01-04 02:12:22 +0000

    generate report from params - 0x00

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,3 +1,9 @@
 export default function createReportObject(employeesList) {
-
+  return ({
+    /* eslint-disable no-unused-vars */
+    allEmployees: employeesList,
+    getNumberOfDepartments(employeeList) {
+      return (Object.keys(employeesList).length);
+    },
+  });
 }
