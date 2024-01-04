a31e55a34aa06710f7a618603323d6b2f5eeb516
Martin <martyofmca@gmail.com>
2024-01-04 02:40:33 +0000

    iterate through report and print contents with a seperator - 0x00

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,3 +1,13 @@
 export default function iterateThroughObject(reportWithIterator) {
+  const reportLength = reportWithIterator.length;
+  let processedReport = 0;
+  let reportStr = '';
 
- }
+  if (!reportWithIterator) return;
+  for (const name of reportWithIterator) {
+    reportStr += name;
+    if ((processedReport + 1) < reportLength) reportStr += ' | ';
+    processedReport += 1;
+  }
+  console.log(reportStr);
+}
