7e798f9c69692a8adc27bd81005bd09333fbaf68
Martin <martyofmca@gmail.com>
2024-01-04 02:25:11 +0000

    return iterator for report - 0x00

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,3 +1,8 @@
 export default function createIteratorObject(report) {
+  const namesReport = [];
 
+  for (const val of Object.values(report.allEmployees)) {
+    namesReport.push(...val);
+  }
+  return (namesReport);
 }
