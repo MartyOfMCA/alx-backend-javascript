99d595c6c341f1b0e7483dbaacd3d1e8b8efcbed
Martin <martyofmca@gmail.com>
2024-01-08 23:32:36 +0000

    return array of objects - 0x03

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,13 @@
+/**
+ * Return an array of students. Each student object
+ * contains their id, first name and location.
+ *
+ * @returns {object} Array of student objects.
+ */
+const getListStudents = () => ([
+  { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+  { id: 2, firstName: 'James', location: 'Columbia' },
+  { id: 5, firstName: 'Serena', location: 'San Francisco' },
+]);
+
+export default getListStudents;
