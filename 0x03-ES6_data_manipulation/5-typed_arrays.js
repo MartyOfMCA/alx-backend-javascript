be993a54965933868ed2b9d270d3dc0a40e1f2d2
Martin <martyofmca@gmail.com>
2024-01-10 00:49:00 +0000

    define a new ArrayBuffer with an Int8 value - 0x03

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,30 @@
+/*
+ * Create a new ArrayBuffer to store an
+ * Int8 value at a specific position.
+ *
+ * @param {number} - The length of the array.
+ * @param {number} - The position of the
+ * inserted value.
+ * @pram {number} - The value to be inserted.
+ *
+ * @returns A new ArrayBuffer with the specified
+ * length and a new value inserted at the
+ * given position.
+ *
+ * @throws Error instance when the given value
+ * cannot be added.
+ */
+/* eslint-disable */
+const createInt8TypedArray = (length, pos, value) => {
+  const arr = new ArrayBuffer(length);
+  const view = new DataView(arr);
+  try {
+    view.setInt8(pos, value);
+  } catch (ex) {
+    new Error('Position outside range');
+  }
+
+  return (arr);
+};
+
+export default createInt8TypedArray;
