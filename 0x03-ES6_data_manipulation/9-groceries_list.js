c0b268b92c4182ed65124f4092a26bee72998d94
Martin <martyofmca@gmail.com>
2024-01-10 02:08:18 +0000

    define a simple Map - 0x03

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,17 @@
+/**
+ * Create a map of grocery store items.
+ *
+ * @returns A map object containing names
+ * and quantity of items.
+ */
+const groceriesList = () => (
+  new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ])
+);
+
+export default groceriesList;
