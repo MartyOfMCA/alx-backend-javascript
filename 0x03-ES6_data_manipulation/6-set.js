3e80d75c017191a96e5dbbb2a44d26bb2f70f83c
Martin <martyofmca@gmail.com>
2024-01-10 01:01:51 +0000

    define a new Set from an array - 0x03

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,14 @@
+/**
+ * Create a new Set from an array
+ *
+ * @pamra {object} - An array of any kind
+ * of elements.
+ *
+ * @returns A new set composed of items
+ * from the given array.
+ */
+const setFromArray = (arr) => (
+  new Set(arr)
+);
+
+export default setFromArray;
