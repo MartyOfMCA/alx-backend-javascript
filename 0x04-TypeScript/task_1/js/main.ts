9234263fe37ce8d69efbde8723bd6dd5d8d0b2f9
Martin <martyofmca@gmail.com>
2024-01-11 00:45:08 +0000

    remove test scripts - 0x04

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,32 +1,13 @@
 interface Teacher {
-    readonly firstName: string,
-    readonly lastName: string,
-    fullTimeEmployee: boolean,
-    yearsOfExperience?: number,
-    location: string,
-    [key: string]: any
+  readonly firstName: string,
+  readonly lastName: string,
+  fullTimeEmployee: boolean,
+  yearsOfExperience?: number,
+  location: string,
+  [key: string]: any
 }
-
-const teacher: Teacher = {
-    firstName: 'Jake',
-    lastName: 'Savage',
-    fullTimeEmployee: true,
-    location: 'Asokwa',
-    contract: true
-};
-
-console.log(teacher);
 
 interface Directors extends Teacher {
-    numberOfReports: number
+  numberOfReports: number
 }
 
-const director: Directors = {
-  firstName: 'Tom',
-  lastName: 'Walker',
-  location: 'Sobolo',
-  fullTimeEmployee: true,
-  numberOfReports: 12,
-};
-
-console.log(director)
