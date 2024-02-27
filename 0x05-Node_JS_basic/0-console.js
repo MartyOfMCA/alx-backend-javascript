a5ba845a33aaa38c2fe6ca78ff9d4cedf1a4a0c5
Martin <martyofmca@gmail.com>
2024-02-27 00:01:50 +0000

    print message to stdout - 0x05

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,11 @@
+/**
+ *
+ * Define a function that prints a given
+ * argument to the standard output stream.
+ *
+ * */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
